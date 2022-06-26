028e85e13d4f3d3ff30484d4e13bf5e339d8c5d1
supriya-mainalli <supriyamainallio@gmail.com>
2022-06-26 14:21:30 +0530

    added basic programs of python

---
+++ basic_programs.py
@@ -0,0 +1,19 @@
+# reverse a string
+mystr = "supriya"
+print(mystr[::-1])  # same applies to reversing list
+
+# reverse a string without any built in method or slicing
+rev_str = " "
+for item in "supriya":
+    rev_str = item + rev_str
+
+print("The reversed string is {}".format(rev_str))
+
+# display only odd index values
+mylist = [10, 13, 11, 45, 89]
+print("The odd index values are {}".format(mylist[::2]))
+
+# print odd or even number in list using list comprehension
+mylist2 = [10, 13, 11, 45, 89, 99, 88]
+mylist2 = [item for item in mylist2 if item % 2 == 0]
+print("The even numbers are {}".format(mylist2))
