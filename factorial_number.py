c71b8bc0a48bc8254a2966be0626c8fe99e737cd
supriya-mainalli <supriyamainallio@gmail.com>
2022-06-26 14:14:33 +0530

    updated factorial_number.py

--- factorial_number.py
+++ factorial_number.py
@@ -1,11 +1,11 @@
 number = int(input("Enter the number"))
 
 
-def fib(n):
+def factorial_number(n):
     if n <= 1:
         return 1
     else:
-        return n * fib(n - 1)
+        return n * factorial_number(n - 1)
 
 
-print(fib(number))
+print(factorial_number(number))
