983c3ffcfabd6f6ab5e0a7a90a210f91d8be5176
supriya-mainalli <supriyamainallio@gmail.com>
2022-08-06 12:54:11 +0530

    string and list

--- interview_questions/global_logic.py
+++ interview_questions/global_logic.py
@@ -3,7 +3,43 @@
 output = [[1,5,0],[2,2,9],[5,1,6]]
 """
 
+
+
+
 """
 2. input: dct_list = ['abc', 'cab', 'abcd', 'dcba', 'abcde']
 output : {3: ['abc', 'cab'], 4: ['abcd', 'dcba'], 5: ['abcde']}
 """
+my_dict = {}
+dct_list = ['abc', 'cab', 'abcd', 'dcba', 'abcde']
+for item in dct_list:
+    if len(item) not in my_dict:
+        my_dict[len(item)] = list([item])
+    else:
+        my_dict[len(item)].append(item)
+
+print(my_dict)
+
+"""
+3.my_str = "My name is supriya M"
+output = "Ma yirp us siemany M"
+"""
+my_str = "My name is supriya M"
+
+
+def reverse_str(my_str):
+    my_list = my_str.split(" ")
+    list2 = []
+    n = 0
+    rev_str = ""
+    for item in my_str:
+        if item != " ":
+            rev_str = rev_str + item
+    rev_str = rev_str[::-1]
+    for item in my_list:
+        list2.append(rev_str[n:n + len(item)])
+        n += len(item)
+    return " ".join(list2)
+
+
+print("the reverse string is ---> {}".format(reverse_str(my_str)))
