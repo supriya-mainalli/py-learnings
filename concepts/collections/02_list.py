76740163c906d87748654a516cdacb8579cb24e1
Supriya Mainalli <supriyamainallio@gmail.com>
2024-10-26 11:40:08 +0530

    update list.py

--- concepts/collections/02_list.py
+++ concepts/collections/02_list.py
@@ -36,20 +36,3 @@ print(names) # ['vinod', 'manju', 'supriya', 'ria', 'maruthi', 'john', 'priya', 
 names = ['vinod', 'manju', 'supriya', 'ria', 'maruthi', 'john', 'priya']
 names.append(['test3'])
 print(names) #['vinod', 'manju', 'supriya', 'ria', 'maruthi', 'john', 'priya', ['test3']]
-
-
-
-""" List comprehensions"""
-
-names = ['vinod', 'manju', 'maruthi', 'ria']
-
-names = [d.upper() for d in names]
-
-
-""" Return value if the item is int or float and the value is even number"""
-
-data = [45, 9874, 'jfgh', 'ufdf', 56,0.98]
-data = [x for x in data if type(x) in (int,float) and x%2==0]
-
-
-print(data, names)
